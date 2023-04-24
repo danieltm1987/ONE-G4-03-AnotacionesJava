801551c2c8c2320ccc8929293bea6edaf152cd04
Daniel Torres Martinez <daniel.tm1987@gmail.com>
2023-04-23 23:47:47 -0500

    commit 02: agregando atributos

--- src/com/anotaciones/clases/Usuario.java
+++ src/com/anotaciones/clases/Usuario.java
@@ -1,6 +1,12 @@
 package com.anotaciones.clases;
 
+import java.time.LocalDate;
+
 public class Usuario {
+	
+	private String nombre;
+	private String identidad;
+	private LocalDate fechaNacimiento;
 	
 
 }
