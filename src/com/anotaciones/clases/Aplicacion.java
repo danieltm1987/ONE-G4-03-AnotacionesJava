7bf43fdfe51b8ff48dd8e20b36ebd4059121554d
Daniel Torres Martinez <daniel.tm1987@gmail.com>
2023-04-24 00:16:07 -0500

    commit 03: se configrua la anotaacion par validar si la edad es mayor a 18 años

---
+++ src/com/anotaciones/clases/Aplicacion.java
@@ -0,0 +1,12 @@
+package com.anotaciones.clases;
+
+import java.time.LocalDate;
+import java.time.Month;
+
+public class Aplicacion {
+
+	public static void main(String[] args) {
+		Usuario usuario = new Usuario("Daniel", "10472182963", LocalDate.of(1987, Month.APRIL, 28));
+		
+	}
+}
--- src/com/anotaciones/clases/Usuario.java
+++ src/com/anotaciones/clases/Usuario.java
@@ -1,12 +1,71 @@
 package com.anotaciones.clases;
 
+import java.lang.reflect.Field;
 import java.time.LocalDate;
+import java.time.Month;
+import java.time.Period;
+
+import com.anotaciones.anotacion.EdadMinima;
 
 public class Usuario {
 	
 	private String nombre;
 	private String identidad;
+	
+	@EdadMinima(valor=18)
 	private LocalDate fechaNacimiento;
 	
+	public Usuario(String nom, String id, LocalDate fecha) {
+		this.nombre = nom;
+		this.identidad = id;
+		this.fechaNacimiento = fecha;
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+
+	public String getIdentidad() {
+		return identidad;
+	}
+
+	public LocalDate getFechaNacimiento() {
+		return fechaNacimiento;
+	}
+
+	public boolean usuarioValido(Usuario usuario){
+		   if(!usuario.getNombre().matches("[a-zA-Záàâãéèêíïóôõöúçñ\\s]+")){
+		      return false;
+		   }
+		   if(!usuario.getIdentidad().matches("[^0-9]+")){
+		      return false;
+		   }
+		   return Period.between(usuario.getFechaNacimiento(), LocalDate.now()).getYears() >= 18;
+	}
+	
+	public static void main(String[] args) {
+		Usuario usuario = new Usuario("Daniel", "10472182963", LocalDate.of(2009, Month.APRIL, 28));
+		System.out.println(validador(usuario));
+	}
+	
+	public static <T> boolean validador(T objeto) {
+		Class<?> clase = objeto.getClass();
+		for (Field field : clase.getDeclaredFields()) {
+			if (field.isAnnotationPresent(EdadMinima.class)) {
+				EdadMinima edadMinima = field.getAnnotation(EdadMinima.class);
+		        try{
+		            field.setAccessible(true);
+		            LocalDate fechaNacimiento = (LocalDate) field.get(objeto);
+		            return Period.between(fechaNacimiento, LocalDate.now()).getYears() >= edadMinima.valor() ;
+		            				
+		         } catch (IllegalAccessException e) {
+		              e.printStackTrace();
+		         }
+		    }
+		}
+		
+		return false;
+		
+	}
 
 }
