ff2f8001f4e4079c50429910d245d160eff11728
Daniel Torres Martinez <daniel.tm1987@gmail.com>
2023-04-24 22:45:24 -0500

    commit 04: Implementanto Usuario

--- src/com/anotaciones/clases/Aplicacion.java
+++ src/com/anotaciones/clases/Aplicacion.java
@@ -7,6 +7,10 @@ public class Aplicacion {
 
 	public static void main(String[] args) {
 		Usuario usuario = new Usuario("Daniel", "10472182963", LocalDate.of(1987, Month.APRIL, 28));
+		System.out.println("Nombre :"+usuario.getNombre());
+		System.out.println("Id :"+usuario.getIdentidad());
+		System.out.println("Fecha :"+usuario.getFechaNacimiento());
+		
 		
 	}
 }
